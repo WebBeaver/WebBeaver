0f12d703459f9e93663778e830bbfaeb751b10c5
Rick Lugtigheid <rlugtigheid77@gmail.com>
2021-11-09 17:05:11 +0100

    Added route attribute and gitignore
    Added a route attribute so the user can assign a method and route to a method and class.
    
    Also added gitignore so I won't push all debug and example builds.

---
+++ WebBeaver/Router.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebBeaver
+{
+	[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+	public class RouteAttribute : Attribute
+	{
+		public static bool HasParams { get; private set; }
+		public string Method { get; }
+		public string Route { get; }
+		/// <summary>
+		/// Create a new route with GET method
+		/// </summary>
+		/// <param name="route"></param>
+		public RouteAttribute(string route)
+		{
+			Method = "GET";
+			Route = route;
+		}
+		/// <summary>
+		/// Create a new route
+		/// </summary>
+		/// <param name="method"></param>
+		/// <param name="route"></param>
+		public RouteAttribute(string method, string route)
+		{
+			Method = method;
+			Route = Route;
+		}
+
+		public bool IsMatch(string route)
+		{
+			// Do simple check first
+			if (Route == route) return true;
+
+			HasParams = true;
+			string[] routeParts = Route.Split('/');
+			string[] inputParts = route.Split('/');
+
+			// Check if every part of the route that isn't a variable is the same 
+			for (int i = 0; i < routeParts.Length; i++)
+				if (!routeParts[i].StartsWith(':') && routeParts[i] != inputParts[i])
+					return false;
+			return true;
+		}
+	}
+
+	public class Router
+	{
+
+	}
+}
